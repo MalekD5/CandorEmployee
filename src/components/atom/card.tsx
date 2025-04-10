ba7ad28e595eb046cef53bdfa45b984c9ff5af11
Malek <106095300+MalekD5@users.noreply.github.com>
2025-04-10 17:06:30 +0300

    fix(card): improper extraction

--- src/components/atom/card.tsx
+++ src/components/atom/card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='<div className="bg-white flex flex-col gap-4 border border-zinc-300 p-10 rounded-xl">'>
+		<div className="bg-white flex flex-col gap-4 border border-zinc-300 p-10 rounded-xl">
 			{children}
 		</div>
 	);

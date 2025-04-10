0dd7519d9b50747838d15575651ce566285c3bac
Malek <106095300+MalekD5@users.noreply.github.com>
2025-04-10 20:03:55 +0300

    style(button): add flex and width full by default

--- src/components/atom/button.tsx
+++ src/components/atom/button.tsx
@@ -4,19 +4,19 @@ import { twMerge } from "tailwind-merge";
 
 import { type VariantProps, tv } from "tailwind-variants";
 
-const buttonVariants = tv({
-	base: "text-white hover:cursor-pointer disabled:cursor-not-allowed active:translate-y-1",
+export const buttonVariants = tv({
+	base: "flex justify-center items-center text-white hover:cursor-pointer disabled:cursor-not-allowed active:translate-y-1 w-full",
 	variants: {
 		color: {
 			default: "bg-accent disabled:bg-blue-800",
-			destructive: "bg-destructive disabled:bg-red-800/90",
+			destructive: "bg-destructive disabled:bg-red-800",
 			warning: "bg-warning disabled:bg-yellow-600/90",
 			success: "bg-success disabled:bg-green-900/90",
 			ghost: "bg-transparent text-black font-bold",
 			outline: "bg-transparent text-black font-bold border-2 border-black",
 		},
 		size: {
-			default: "px-4 py-2",
+			default: "px-4 py-3",
 			sm: "px-2 py-1 text-sm",
 			lg: "px-6 py-3 text-xl",
 			xl: "px-8 py-4 text-2xl",

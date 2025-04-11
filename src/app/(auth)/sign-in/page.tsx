1ee1627a1d20385d14647117b91f4bc45ee430d0
Malek <106095300+MalekD5@users.noreply.github.com>
2025-04-12 00:26:49 +0300

    fix: wrap useSearchParam in suspense

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,37 +2,40 @@
 
 import SignInTemplate from "@/components/template/sign-in-template";
 import { signIn } from "@/lib/auth-client";
+import { Suspense } from "react";
 import { toast } from "react-toastify";
 
 export default function SignInPage() {
 	return (
-		<SignInTemplate
-			onSignIn={async (e, setLoading, router) => {
-				e.preventDefault();
-				const formData = new FormData(e.currentTarget);
-				const { email, password } = Object.fromEntries(formData) as {
-					email: string;
-					password: string;
-				};
+		<Suspense>
+			<SignInTemplate
+				onSignIn={async (e, setLoading, router) => {
+					e.preventDefault();
+					const formData = new FormData(e.currentTarget);
+					const { email, password } = Object.fromEntries(formData) as {
+						email: string;
+						password: string;
+					};
 
-				setLoading(true);
+					setLoading(true);
 
-				await signIn.email({
-					email,
-					password,
-					fetchOptions: {
-						onSuccess: () => {
-							router.replace("/");
+					await signIn.email({
+						email,
+						password,
+						fetchOptions: {
+							onSuccess: () => {
+								router.replace("/");
+							},
+							onError: () => {
+								toast.error("Invalid email or password");
+							},
+							onResponse: () => {
+								setLoading(false);
+							},
 						},
-						onError: () => {
-							toast.error("Invalid email or password");
-						},
-						onResponse: () => {
-							setLoading(false);
-						},
-					},
-				});
-			}}
-		/>
+					});
+				}}
+			/>
+		</Suspense>
 	);
 }
